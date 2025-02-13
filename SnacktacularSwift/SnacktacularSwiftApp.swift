cbda1d6796801567820f96e6a707a0664e4c6cb2
oleh <phd.chornyi@gmail.com>
2025-02-13 10:35:12 +0200

    Initial Commit

---
+++ SnacktacularSwift/SnacktacularSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularSwiftApp.swift
+//  SnacktacularSwift
+//
+//  Created by Oleh on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
